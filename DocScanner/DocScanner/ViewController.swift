cd85984623e0ef82aa2692c702b469cee10bbccb
Samuel Tse <xiezhijian15@gmail.com>
2019-04-26 21:57:45 +0800

    initial commit
    Created a empty workspace project

---
+++ DocScanner/DocScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DocScanner
+//
+//  Created by Samuel Tse on 22/4/19.
+//  Copyright © 2019 Samuel Tse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DocScanner/Pods/Target Support Files/Pods-DocScannerUITests/Pods-DocScannerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DocScannerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DocScannerUITests
+@end
